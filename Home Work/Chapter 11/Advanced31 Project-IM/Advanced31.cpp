47d30741fafb700693ee5847eeb744263b489251
John Brosius <brosius01@gmail.com>
2020-04-06 22:58:09 -0500

    update chapter11

---
+++ Home Work/Chapter 11/Advanced31 Project-IM/Advanced31.cpp
@@ -0,0 +1,24 @@
+//Advanced31.cpp - displays a grade based 
+//on the total points entered by the user
+//Created/revised by <your name> on <current date>
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//declare variables
+	int sub    = 0;    //keeps track of subscripts
+	int points = 0;  
+
+	//declare arrays and fill with data
+	
+	//get total points
+	cout << "Enter points earned (negative number to stop program): ";
+	cin >> points;
+	
+	
+
+	//system("pause");
+	return 0;
+}	//end of main function
---
+++ Home Work/Chapter 11/README.md
@@ -0,0 +1,3 @@
+# Chapter-11
+# Get your book
+<h3>Objectives</h3>
